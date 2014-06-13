0e08cce9c595205227fc83ea76c971e0d6a1dc52
quinston <lklq@live.com>
2014-06-13 08:42:58 -0400

    Write sonar sensor function, returns distance in centimetres

--- scurryant.ino
+++ scurryant.ino
@@ -10,8 +10,39 @@ void setup() {
   Serial.begin(9600);
 }
 
+int timeToCm(int t) {
+  //http://www.crcibernetica.com/ultrasonic-range-finder-sdm-io/
+  return (t*340.23/2/10000)-3;
+}
+
+/* Returns distance of remote obstacle in cm */
+int echolocate() {
+  int trig = 2, gnd = 3, fiveV = 4;
+  
+  /* Power the pins */
+  pinMode(fiveV, OUTPUT);
+  digitalWrite(fiveV, HIGH);
+  pinMode(gnd, OUTPUT);
+  digitalWrite(gnd, LOW);
+  
+  
+  /* Do some location */
+  
+  
+  pinMode(trig , OUTPUT);
+  digitalWrite(trig, LOW);
+  delay(25);
+  digitalWrite(trig, HIGH);
+  int time = micros();
+  pinMode(trig, INPUT);
+  int pulseSize = pulseIn(trig, LOW, 18000); //expect 15us, timeout 18ms
+  time = micros() - time - pulseSize; // time since sending the pulse and receiving the whole pulse
+  return timeToCm(time);  
+}
+
 void loop() {
-  int a = analogRead(photoresistor);
+  /* int a = analogRead(photoresistor); */
+  int a  = echolocate();
   Serial.println(a);
 
   if (a < darknessCeiling) {
@@ -31,6 +62,6 @@ void loop() {
     }
   }
 
-  delay(200);
+  delay(50);
 }
 
